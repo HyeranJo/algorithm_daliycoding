ce787ca20c828ecaf200213a70dd3c97e639c601
johyeran <a01026823164@gmail.com>
2023-04-07 10:42:01 +0900

    ComputeWhenDouble

---
+++ src/ComputeWhenDouble.java
@@ -0,0 +1,20 @@
+public class ComputeWhenDouble {
+    public static void main(String[] args) {
+        // TODO : 연이율을 입력받아 원금이 2배 이상 될 때까지 걸리는 시간 반환
+        int output = computeWhenDouble(7);
+        System.out.println(output); // --> 11
+
+        output = computeWhenDouble(10);
+        System.out.println(output); // --> 8
+    }
+    public static int computeWhenDouble(double interestRate) {
+        double rate = 1 + interestRate / 100; // 1.05
+        int year = 0;
+        double money = 1;
+        while(money < 2) {
+            money *= rate;
+            ++year;
+        }
+        return year;
+    }
+}
